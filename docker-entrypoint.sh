cb6da317ca79f1e521f14bcedfd4568b98d101f5
hchen549 <hchen549@wisc.edu>
2024-01-30 04:55:36 +0000

    move the wait db logic to docker compose

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,10 +2,6 @@
 # Abort on any error (including if wait-for-it fails).
 set -e
 
-echo "wait for postgres"
-/app/wait-for-it.sh "db:5432" 
-echo "postgres is up"
-
 echo "run db migration"
 /app/migrate -path /app/migration -database $DB_SOURCE -verbose up
 echo "db migration done"
